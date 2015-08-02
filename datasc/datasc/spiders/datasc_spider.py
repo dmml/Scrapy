a4f8ea5ef059bcf463d96d30525ecd5977caf231
dmml <zxa2012@gmail.com>
2015-08-01 23:06:56 -0500

    format change

--- datasc/datasc/spiders/datasc_spider.py
+++ datasc/datasc/spiders/datasc_spider.py
@@ -14,7 +14,6 @@ class Datasc_spider(CrawlSpider):
     start_urls = [
         "http://www.datasciencecentral.com/profiles/blog/list"
     ]
-
     rules = (
         # Extract links matching 'category.php' (but not matching 'subsection.php')
         # and follow links from them (since no callback means follow=True by default).
